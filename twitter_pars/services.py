2b05f966b31472b891d097aa12be6cb90284475f
Anton Salamov <gracioso@pm.me>
2022-10-19 22:54:11 +0300

    Add proxy

--- twitter_pars/services.py
+++ twitter_pars/services.py
@@ -1,10 +1,10 @@
+from random import randint
 import tweepy
 from dotenv import load_dotenv
 import os
 
 
 load_dotenv()
-
 
 auth = tweepy.OAuth1UserHandler(
    os.getenv('API_KEY'),
@@ -14,11 +14,21 @@ auth = tweepy.OAuth1UserHandler(
 )
 auth_bearer = tweepy.OAuth2BearerHandler(f"Bearer {os.getenv('API_KEY')}")
 
-api = tweepy.API(auth)
+
+def get_random_proxy():
+    proxy = [
+        'http://uJEM1BHn:HBPjf7Tc@212.193.143.51:48707',
+        'http://Q8xxGmcG:56CSHWxE@91.188.221.249:45460',
+        'http://Q8xxGmcG:56CSHWxE@195.208.123.248:48634'
+    ]
+    count = len(proxy)
+    return proxy[count - 1]
 
 
 def get_user_data(name=''):
+    api = tweepy.API(auth, proxy=get_random_proxy())
     user = api.get_user(screen_name=name.replace('\r\n', ''))
+
     data = {
         'id': user.id,
         'name': user.name,
@@ -31,8 +41,12 @@ def get_user_data(name=''):
 
 
 def get_user_timeline(user_id=''):
+    api = tweepy.API(auth, proxy=get_random_proxy())
     public_tweets = api.user_timeline(user_id=user_id)
     lists = []
-    for tweet in public_tweets:
-        lists.append(tweet.text)
+    try:
+        for tweet in public_tweets:
+            lists.append(tweet.text)
+    except:
+        pass
     return lists
